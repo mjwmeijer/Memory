08aec338533d4978805783671d1fc404dd9e93f2
mjwmeijer <mjwmeijer@yahoo.com>
2017-11-06 13:34:38 +0100

    viewport changes.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -95,10 +95,10 @@ console.log($(newCard).data('isFlipped'))
 
 $($game).append(newCard);
 }
-/*
+
 var cardWidth = $('.card').outerWidth();
 $('.card').css('height',cardWidth);
-*/
+
 $('.card').click(function() {
   MatchGame.flipCard($(this), $('#game'));
   });
